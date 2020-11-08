738c0ce1ae7f02f0e98ce702435c166ac15b1674
admin <admin@imac.local>
2020-11-08 23:23:27 +0300

    xxx

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,11 @@
 # Container image that runs your code
-FROM alpine:3.10
+FROM ubuntu:latest
 
 # Copies your code file from your action repository to the filesystem path `/` of the container
-COPY entrypoint.sh /entrypoint.sh
-RUN chmod +x entrypoint.sh
-COPY telegram.sh /telegram.sh
-RUN chmod +x telegram.sh
+ADD entrypoint.sh /entrypoint.sh
+# RUN chmod +x entrypoint.sh
+ADD telegram.sh /telegram.sh
+# RUN chmod +x telegram.sh
 
 # Code file to execute when the docker container starts up (`entrypoint.sh`)
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,32 +3,32 @@
 echo "Starting..."
 echo $arg
 
-FAIL_CODE=6
+# FAIL_CODE=6
 
-LRED="\033[1;31m"   # Light Red
-LGREEN="\033[1;32m" # Light Green
-NC='\033[0m'        # No Color
+# LRED="\033[1;31m"   # Light Red
+# LGREEN="\033[1;32m" # Light Green
+# NC='\033[0m'        # No Color
 
-status=0
+# status=0
 
-check_status() {
+# check_status() {
 
-  curl -sf "${1}" >/dev/null
+#   curl -sf "${1}" >/dev/null
 
-  if [ ! $? = ${FAIL_CODE} ]; then
-    echo -e "${LGREEN}${1} is online${NC}"
-    ./telegram.sh -t "${TELEGRAM_TOKEN}" -c "${TELEGRAM_CHAT_ID}" "${1} is online"
-  else
-    echo -e "${LRED}${1} is down${NC}"
-    ./telegram.sh -t "${TELEGRAM_TOKEN}" -c "${TELEGRAM_CHAT_ID}" "${1} is down"
-    status=1
-  fi
-}
+#   if [ ! $? = ${FAIL_CODE} ]; then
+#     echo -e "${LGREEN}${1} is online${NC}"
+#     ./telegram.sh -t "${TELEGRAM_TOKEN}" -c "${TELEGRAM_CHAT_ID}" "${1} is online"
+#   else
+#     echo -e "${LRED}${1} is down${NC}"
+#     ./telegram.sh -t "${TELEGRAM_TOKEN}" -c "${TELEGRAM_CHAT_ID}" "${1} is down"
+#     status=1
+#   fi
+# }
 
-for arg; do
-  check_status "$arg"
-done
+# for arg; do
+#   check_status "$arg"
+# done
 
-if (( status )); then
-  exit 1
-fi
+# if (( status )); then
+#   exit 1
+# fi
