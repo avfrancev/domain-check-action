6031b0c76bc653891568f8773c2152a871d61fc7
admin <admin@imac.local>
2020-11-08 22:37:23 +0300

    init

---
+++ entrypoint.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+FAIL_CODE=6
+
+LRED="\033[1;31m"   # Light Red
+LGREEN="\033[1;32m" # Light Green
+NC='\033[0m'        # No Color
+
+status=0
+
+check_status() {
+
+  curl -sf "${1}" >/dev/null
+
+  if [ ! $? = ${FAIL_CODE} ]; then
+    echo -e "${LGREEN}${1} is online${NC}"
+    ./telegram.sh -t "${TELEGRAM_TOKEN}" -c "${TELEGRAM_CHAT_ID}" "${1} is online"
+  else
+    echo -e "${LRED}${1} is down${NC}"
+    ./telegram.sh -t "${TELEGRAM_TOKEN}" -c "${TELEGRAM_CHAT_ID}" "${1} is down"
+    status=1
+  fi
+}
+
+for arg; do
+  check_status "$arg"
+done
+
+if (( status )); then
+  exit 1
+fi
